789280b8c58fad9d1d505b192a11a13442b46605
Lance Terrence <magsombol.lance@gmail.com>
2020-12-11 15:23:34 +0800

    Added SVGs and main javascript file, also added logo section

---
+++ js/main.js
@@ -0,0 +1,30 @@
+var slideCount = 1;
+
+    function myFunction(x) {
+
+        if (x.matches) { // If media query matches
+            return;
+        } else {
+            slideCount = 3;
+        }
+    }
+
+    var x = window.matchMedia("(max-width: 600px)")
+    myFunction(x); // Call listener function at run time
+    x.addListener(myFunction); // Attach listener function on state changes
+
+
+    var swiper = new Swiper('.swiper-container', {
+        slidesPerView: slideCount,
+        spaceBetween: 0,
+        freeMode: true,
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+        },
+        // Navigation arrows
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+    });
